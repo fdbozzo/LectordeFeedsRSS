634d3176a3f50262b50b8425cbd7f02af6985c32
Fernando D. Bozzo <fdbozzo@gmail.com>
2020-12-27 03:49:31 +0100

    Agregado borrado ROOM y carga inicial de grupos, feeds y channels en ROOM.

--- app/src/main/java/com/blogspot/fdbozzo/lectorfeedsrss/MainSharedViewModel.kt
+++ app/src/main/java/com/blogspot/fdbozzo/lectorfeedsrss/MainSharedViewModel.kt
@@ -3,6 +3,11 @@ package com.blogspot.fdbozzo.lectorfeedsrss
 import android.content.Context
 import androidx.lifecycle.*
 import com.blogspot.fdbozzo.lectorfeedsrss.data.RssResponse
+import com.blogspot.fdbozzo.lectorfeedsrss.data.database.FeedDatabase
+import com.blogspot.fdbozzo.lectorfeedsrss.data.database.feed.FeedChannel
+import com.blogspot.fdbozzo.lectorfeedsrss.data.domain.feed.Group as DomainGroup
+import com.blogspot.fdbozzo.lectorfeedsrss.data.domain.feed.Feed as DomainFeed
+import com.blogspot.fdbozzo.lectorfeedsrss.data.domain.feed.FeedChannel as DomainFeedChannel
 import com.blogspot.fdbozzo.lectorfeedsrss.data.domain.FeedRepository
 import com.blogspot.fdbozzo.lectorfeedsrss.network.feed.Feed
 import com.blogspot.fdbozzo.lectorfeedsrss.ui.feed.RssApiStatus
@@ -16,7 +21,7 @@ class MainSharedViewModel(private val feedRepository: FeedRepository) : ViewMode
 
     private lateinit var rssApiResponse: RssResponse<Feed>
 
-    private var apiBaseUrl = "https://hardzone.es/" // "http://blog.mozilla.com/" // "https://hardzone.es/"
+    private var apiBaseUrl = "https://hardzone.es" // "http://blog.mozilla.com/" // "https://hardzone.es/"
 
     /*
     private var _channels = MutableLiveData<List<DomainFeedChannel>>()
@@ -52,14 +57,20 @@ class MainSharedViewModel(private val feedRepository: FeedRepository) : ViewMode
      * Llamar a getRssFeedData() en el init para obtener los datos inmediatamente.
      */
     init {
+
+        // Cargamos datos iniciales en el drawer
+        viewModelScope.launch { setupInitialDrawerMenuData() }
+
+
         viewModelScope.launch {
+            //setupInitialDrawerMenuData()
 
             rssApiResponse = feedRepository.checkNetworkFeeds(apiBaseUrl)
 
             when (rssApiResponse) {
                 is RssResponse.Success -> {
                     // TODO: Falta filtrar los items leidos antes de actualizar el LiveData
-                    Timber.d("RssResponse.Success!}")
+                    Timber.d("RssResponse.Success!")
                 }
                 is RssResponse.Error -> {
                     // TODO: Falta controlar errores
@@ -72,6 +83,98 @@ class MainSharedViewModel(private val feedRepository: FeedRepository) : ViewMode
     override fun onCleared() {
         Timber.i("onCleared() - mainSharedViewModel.fragmento: %s", testigo)
         super.onCleared()
+    }
+
+    private suspend fun setupInitialDrawerMenuData() {
+        // TODO: implementar carga inicial de datos del menú Drawer.
+        //val localDatabase = FeedDatabase.getInstance(context)
+        // Borro la bbdd
+        feedRepository.deleteAllLocalGroups()
+
+        val gId1 = feedRepository.saveLocalGroup(DomainGroup()) // "Uncategorized"
+        val fId1 = feedRepository.saveLocalFeed(
+            DomainFeed(
+                groupId = gId1,
+                linkName = "Stéphane Graber's website",
+                link = "https://stgraber.org"
+            )
+        )
+        val cId1 = feedRepository.saveLocalFeedChannel(
+            DomainFeedChannel(
+                feedId = fId1,
+                title = "Stéphane Graber's website",
+                description = "Stéphane Graber's Feed",
+                link = "https://stgraber.org/feed"
+            )
+        )
+
+        val gId2 = feedRepository.saveLocalGroup(DomainGroup(groupName = "Tecnología y Ciencia"))
+        val fId2 = feedRepository.saveLocalFeed(
+                DomainFeed(
+                    groupId = gId2,
+                    linkName = "HardZone",
+                    link = "https://hardzone.es"
+                )
+            )
+        val cId2 = feedRepository.saveLocalFeedChannel(
+            DomainFeedChannel(
+                feedId = fId2,
+                title = "HardZone",
+                description = "HardZone Feed",
+                link = "https://hardzone.es"
+            )
+        )
+
+        val gId3 = feedRepository.saveLocalGroup(DomainGroup(groupName = "Hogar"))
+        val fId3 = feedRepository.saveLocalFeed(
+            DomainFeed(
+                groupId = gId3,
+                linkName = "EcoInventos",
+                link = "https://ecoinventos.com"
+            )
+        )
+        val cId3 = feedRepository.saveLocalFeedChannel(
+            DomainFeedChannel(
+                feedId = fId3,
+                title = "EcoInventos",
+                description = "EcoInventos Feed",
+                link = "https://ecoinventos.com"
+            )
+        )
+
+        val gId4 = feedRepository.saveLocalGroup(DomainGroup(groupName = "Android"))
+        val fId4 = feedRepository.saveLocalFeed(
+            DomainFeed(
+                groupId = gId4,
+                linkName = "Android Police",
+                link = "https://www.androidpolice.com"
+            )
+        )
+        val cId4 = feedRepository.saveLocalFeedChannel(
+            DomainFeedChannel(
+                feedId = fId4,
+                title = "Android Police",
+                description = "Android Police Feed",
+                link = "https://www.androidpolice.com"
+            )
+        )
+
+        val gId5 = feedRepository.saveLocalGroup(DomainGroup(groupName = "Hardware"))
+        val fId5 = feedRepository.saveLocalFeed(
+            DomainFeed(
+                groupId = gId5,
+                linkName = "El Chapuzas Informático",
+                link = "https://elchapuzasinformatico.com"
+            )
+        )
+        val cId5 = feedRepository.saveLocalFeedChannel(
+            DomainFeedChannel(
+                feedId = fId5,
+                title = "El Chapuzas Informático",
+                description = "El Chapuzas Informático Feed",
+                link = "https://elchapuzasinformatico.com"
+            )
+        )
     }
 
     class Factory(private val context: Context, private val feedRepository: FeedRepository) : ViewModelProvider.Factory {
