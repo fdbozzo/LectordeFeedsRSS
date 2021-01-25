7b980e3423d2e8037f51bd2be46ac18ebcc9e25a
Fernando D. Bozzo <fdbozzo@gmail.com>
2021-01-25 22:22:34 +0100

    Agregadas algunas cabeceras HTTP en llamadas Retrofit para evitar algunos errores en algunas webs, como www.androidpolice.com

--- app/src/main/java/com/blogspot/fdbozzo/lectorfeedsrss/network/RetrofitFactory.kt
+++ app/src/main/java/com/blogspot/fdbozzo/lectorfeedsrss/network/RetrofitFactory.kt
@@ -1,8 +1,12 @@
 package com.blogspot.fdbozzo.lectorfeedsrss.network
 
 import com.blogspot.fdbozzo.lectorfeedsrss.data.database.FeedDatabase
+import okhttp3.Headers
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
+import okio.IOException
 import retrofit2.Retrofit
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory
 import timber.log.Timber
@@ -23,11 +27,6 @@ abstract class RetrofitFactory {
         private var builder: Retrofit.Builder = Retrofit.Builder()
             //.addConverterFactory(SimpleXmlConverterFactory.create())
             .baseUrl(BASE_URL)
-
-        //private lateinit var rssApiService: RssApiService
-
-        //@Volatile
-        //private var INSTANCE: RetrofitFactory? = null
 
         @Volatile
         private var rssApiService: RssApiService? = null
@@ -60,15 +59,17 @@ abstract class RetrofitFactory {
 
         private fun makeOkHttpClient(): OkHttpClient {
             return OkHttpClient.Builder()
-                //.addInterceptor(makeLoggingInterceptor())
                 .connectTimeout(120, TimeUnit.SECONDS)
                 .readTimeout(120, TimeUnit.SECONDS)
                 .writeTimeout(90, TimeUnit.SECONDS)
+                //.addInterceptor(getHeaderInterceptor())
+                .addInterceptor(makeLoggingInterceptor())
                 .build()
         }
 
         private fun makeLoggingInterceptor(): HttpLoggingInterceptor {
             val logging = HttpLoggingInterceptor()
+            //logging.level = HttpLoggingInterceptor.Level.BODY
             logging.level = HttpLoggingInterceptor.Level.BODY
             return logging
         }
@@ -81,6 +82,21 @@ abstract class RetrofitFactory {
                 .client(makeOkHttpClient())
                 .addConverterFactory(SimpleXmlConverterFactory.create())
             //.build() //.create(RssApiService::class.java)
+        }
+
+        fun getHeaderInterceptor(): Interceptor {
+            return object : Interceptor {
+                @Throws(IOException::class)
+                override fun intercept(chain: Interceptor.Chain): Response {
+                    val request = chain.request().newBuilder()
+                            .header("Cache-Control", "no-cache")
+                            .header("Connection", "keep-alive")
+                            .header("User-Agent", "OkHttpClient/3")
+                            .header("Accept-Encoding", "*/*")
+                            .build()
+                    return chain.proceed(request)
+                }
+            }
         }
 
         /*
--- app/src/main/java/com/blogspot/fdbozzo/lectorfeedsrss/network/RssApiService.kt
+++ app/src/main/java/com/blogspot/fdbozzo/lectorfeedsrss/network/RssApiService.kt
@@ -3,29 +3,26 @@ package com.blogspot.fdbozzo.lectorfeedsrss.network
 import com.blogspot.fdbozzo.lectorfeedsrss.network.feed.Feed as ServerFeed
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 enum class RssApiStatus { LOADING, ERROR, DONE }
 
 interface RssApiService {
 
-    //@GET("/feed")
+    @Headers(
+        //"Cache-Control: no-cache",
+        "User-Agent: LectorDeFeedsRSS/1.0",
+        "Connection: keep-alive",
+        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding: gzip, deflate, br",
+        //"Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3"
+    )
     @GET("/feed")
     //fun feed(): Call<Rss>
     suspend fun getRss(): Response<ServerFeed>
-    //suspend fun getRss(): List<Feed>
 }
 
 fun rssApi(apiBaseUrl: String): RssApiService {
-    //val retrofitService : RssApiService by lazy { RetrofitFactory.makeRetrofitService() }
     val retrofitService : RssApiService = RetrofitFactory.getRssApiService(apiBaseUrl)
     return retrofitService
 }
-
-/**
- * A public Api object that exposes the lazy-initialized Retrofit service
- */
-/*
-object RssApi {
-    val retrofitService : RssApiService by lazy { RetrofitFactory.makeRetrofitService() }
-}
- */
