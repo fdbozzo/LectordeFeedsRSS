705eda2f0192fcd573335df7d14630babb4430d8
Fernando D. Bozzo <fdbozzo@gmail.com>
2021-01-26 23:32:26 +0100

    Arreglo de cabeceras para Retrofit, para evitar problemas con algunos XML mal interpretados (el "Accept-Encoding" no se debe poner)

--- app/src/main/java/com/blogspot/fdbozzo/lectorfeedsrss/network/RssApiService.kt
+++ app/src/main/java/com/blogspot/fdbozzo/lectorfeedsrss/network/RssApiService.kt
@@ -11,11 +11,13 @@ interface RssApiService {
 
     @Headers(
         //"Cache-Control: no-cache",
-        "User-Agent: LectorDeFeedsRSS/1.0",
-        "Connection: keep-alive",
+        //"Connection: keep-alive",
         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "Accept-Encoding: gzip, deflate, br",
+        //"Accept-Encoding: gzip, deflate, br",  // Da problemas con los XML
+        //"Content-Type: text/xml",
+        //"Accept-Charset: utf-8"
         //"Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3"
+        "User-Agent: LectorDeFeedsRSS/1.0"
     )
     @GET("/feed")
     //fun feed(): Call<Rss>
